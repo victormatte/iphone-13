51bb49e18076f51dccb8327656bf91ba16e90ef1
victormatte <144286725+victormatte@users.noreply.github.com>
2023-10-03 18:59:47 -0300

    iphone landing page no responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+function clicou (event){
+    document.querySelector('#iphone-pic').src = event;
+}
+
+//background change//
+
+function circlechange(bg){
+    let circle = document.querySelector('.circle');
+    circle.style.background = bg;
+}
